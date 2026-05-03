c67377015ba04da25ab7f740daea6d21293b1bcb
Mahipal Panwar <mahipalpanwar6354@gmail.com>
2026-05-03 19:16:08 +0530

    Routing

--- main.go
+++ main.go
@@ -2,47 +2,155 @@ package main
 
 import (
 	"encoding/json"
+	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // model for course - file
 
-type Course struct{
- CourseId string `json:"courseid"`
- CourseName string `json:"coursename"`
- CoursePrise string `json:"price"`
- Author *Author `json:"author"`
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrise int     `json:"price"`
+	Author      *Author `json:"author"`
 }
 
-
-type Author struct{
-Fullname string `json:"fullname"`
-Website string `json:"website"`
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
 }
 
-// fake Database 
+// fake Database
 
 var courses []Course
 
 // middleware
 
-func (c *Course) IsEmpty() bool{
-	return c.CourseId=="" && c.CourseName==""
+func (c *Course) IsEmpty() bool {
+	// return c.CourseId=="" && c.CourseName==""
+	return c.CourseName == ""
 }
 
-// controllers 
+// controllers
 
-// home route 
+// home route
 
-func serveHome(w http.ResponseWriter, r *http.Request){
+func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>welcome to the Course"))
 }
 
-func getAllCourses(w  http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
 }
 
-func main(){
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Contetn-Type", "application/json")
 
+	// grab id from request
+	params := mux.Vars(r)
+
+	// loop through courses, find matching id and return the response
+
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No course found with given id")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// if body is empty
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("please send some data")
+	}
+
+	// about - {}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+
+	// generate unique id, string
+	// append course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+}
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from the request
+
+	params := mux.Vars(r)
+
+	// loop,id,remove,add with my ID
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	// when id is not find
+	json.NewEncoder(w).Encode("invalid id")
+	return
+}
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	// loop , id , remove (index,index+1)
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// seeding
+	courses = append(courses, Course{CourseId: "2", CourseName: "Golang", CoursePrise: 299, Author: &Author{Fullname: "mahipal panwar", Website: "CodeStudy.com"}})
+	courses = append(courses, Course{CourseId: "3", CourseName: "Golang DSA", CoursePrise: 399, Author: &Author{Fullname: "mahipal panwar", Website: "CodeStudy.com"}})
+
+	// Routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/course{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createOneCourse).Methods("POST")
+	r.HandleFunc("/course{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/course{}", deleteOneCourse).Methods("DELETE")
+
+	// listening to a port
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
